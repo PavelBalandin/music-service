d34018f399ce267d9f85a1fd7afc6fe1a718eb36
PashaBalandin <pashabalandin1200@gmail.com>
2021-04-24 13:06:51 +0300

    Spring security has been added

--- src/main/java/com/music/MusicApplication.java
+++ src/main/java/com/music/MusicApplication.java
@@ -2,12 +2,13 @@ package com.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MusicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
 
 }
